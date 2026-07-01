a9c19aa81df6153f4083aead80519be3b8c598f8
Surendra Ghimire <surendra.ghimire@annofi.com>
2026-07-01 17:27:57 +0545

    imporved guide-line boarder

--- src/app/components/ultree/ultree.ts
+++ src/app/components/ultree/ultree.ts
@@ -1,86 +1,85 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ɵEmptyOutletComponent } from "@angular/router";
 
 @Component({
   selector: 'app-ultree',
-  imports: [ɵEmptyOutletComponent],
+  imports: [],
   templateUrl: './ultree.html',
   styleUrl: './ultree.scss',
 })
 export class Ultree {
 
-   @ViewChild('treeBody')
-  treeBody!: ElementRef<HTMLElement>;
+//    @ViewChild('treeBody')
+//   treeBody!: ElementRef<HTMLElement>;
 
-  @ViewChild('verticalLine')
-  verticalLine!: ElementRef<HTMLElement>;
-    private resizeObserver?: ResizeObserver;
-    private observer!: MutationObserver;
+//   @ViewChild('verticalLine')
+//   verticalLine!: ElementRef<HTMLElement>;
+//     private resizeObserver?: ResizeObserver;
+//     private observer!: MutationObserver;
 
-;
+// ;
 
-  ngAfterViewInit(): void {
-    this.updateLineHeight();
-    this.setHorizontalLine();
+//   ngAfterViewInit(): void {
+//     this.updateLineHeight();
+//     this.setHorizontalLine();
 
-    this.resizeObserver = new ResizeObserver(() => {
-      this.updateLineHeight();
-    });
+//     this.resizeObserver = new ResizeObserver(() => {
+//       this.updateLineHeight();
+//     });
 
-    this.resizeObserver.observe(this.treeBody.nativeElement);
+//     this.resizeObserver.observe(this.treeBody.nativeElement);
 
-    this.observer = new MutationObserver(() => {
-      this.setHorizontalLine();
-    });
+//     this.observer = new MutationObserver(() => {
+//       this.setHorizontalLine();
+//     });
 
-    this.observer.observe(this.treeBody.nativeElement, {
-      childList: true,
-      subtree: true
-    });
-  }
+//     this.observer.observe(this.treeBody.nativeElement, {
+//       childList: true,
+//       subtree: true
+//     });
+//   }
 
-  private updateLineHeight(): void {
-    const body = this.treeBody.nativeElement;
-    const line = this.verticalLine.nativeElement;
+//   private updateLineHeight(): void {
+//     const body = this.treeBody.nativeElement;
+//     const line = this.verticalLine.nativeElement;
 
-    let lastChild = body.lastElementChild?.lastElementChild as HTMLElement;
+//     let lastChild = body.lastElementChild?.lastElementChild as HTMLElement;
     
-    if (lastChild?.tagName.toLowerCase() === 'app-ultree') {
-      lastChild = lastChild
-      .querySelector('details > summary') as HTMLElement;
-    }
+//     if (lastChild?.tagName.toLowerCase() === 'app-ultree') {
+//       lastChild = lastChild
+//       .querySelector('details > summary') as HTMLElement;
+//     }
 
-    if (!lastChild) {
-      return;
-    }
+//     if (!lastChild) {
+//       return;
+//     }
 
-    const bodyRect = body.getBoundingClientRect();
-    const lastRect = lastChild.getBoundingClientRect();
+//     const bodyRect = body.getBoundingClientRect();
+//     const lastRect = lastChild.getBoundingClientRect();
 
-    const height = (lastRect.top - bodyRect.top) + (lastRect.height / 2);
+//     const height = (lastRect.top - bodyRect.top) + (lastRect.height / 2);
 
-    line.style.height = `${height}px`;
-  }
+//     line.style.height = `${height}px`;
+//   }
 
-  private setHorizontalLine(): void {
-    const body = this.treeBody.nativeElement.lastElementChild;
+//   private setHorizontalLine(): void {
+//     const body = this.treeBody.nativeElement.lastElementChild;
     
     
-    if (body) {
-      Array.from(body.children).forEach((child) => {
-        const el = child as HTMLElement;
-        console.log(el);
+//     if (body) {
+//       Array.from(body.children).forEach((child) => {
+//         const el = child as HTMLElement;
+//         console.log(el);
   
-        if (el.tagName.toLowerCase() === 'app-ultree') {
-          const summary = el.querySelector('details > summary') as HTMLElement;
+//         if (el.tagName.toLowerCase() === 'app-ultree') {
+//           const summary = el.querySelector('details > summary') as HTMLElement;
   
-          if (summary) {
-            summary.classList.add('tree-horizontal-line');
-          }
-        } else {
-          el.classList.add('tree-horizontal-line');
-        }
-      });
-    }
-  }
+//           if (summary) {
+//             summary.classList.add('tree-horizontal-line');
+//           }
+//         } else {
+//           el.classList.add('tree-horizontal-line');
+//         }
+//       });
+//     }
+//   }
 }
